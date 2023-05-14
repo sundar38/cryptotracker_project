fb5a8b6d91e7ca0610c43d08628da8c9f97ef684
Penugonda <sundara.k.penugonda@accenture.com>
2023-05-14 12:28:14 +0530

    grid, list

--- src/Components/Dashboard/TabsComponent/index.js
+++ src/Components/Dashboard/TabsComponent/index.js
@@ -6,6 +6,8 @@ import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import { useState } from 'react';
 import "./style.css"
+import TrendingDownRoundedIcon from '@mui/icons-material/TrendingDownRounded';
+import TrendingUpRoundedIcon from '@mui/icons-material/TrendingUpRounded';
 export default function TabsComponent({coins}) {
   const [value, setValue] =useState('1');
 
@@ -37,10 +39,21 @@ export default function TabsComponent({coins}) {
                                 <p className='names'>{coin.name}</p>
                             </div>
                             </div>
-                            <div>
+                            {coin.price_change_percentage_24h>0 ?
+                                <div className='numbergroup'>
+                                  <p className='percent greencol'>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+                                  <div><TrendingUpRoundedIcon/></div>
+                                </div>
+                                :
+                                <div className='numbergroup'>
+                                  <p className='percent redcol'>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+                                  <div><TrendingDownRoundedIcon/></div>
+                                </div>
+                            }                            
+                            <div >
                                 <p>Total Volume: {coin.total_volume}</p>
-                                <p>Market Cap: {coin.market_cap}</p>
-                            </div>
+                                <p>Market Cap: ${coin.market_cap}</p>
+                              </div>
                         </div>
                     </div>
                   )})
@@ -48,10 +61,29 @@ export default function TabsComponent({coins}) {
             </div>
         </TabPanel>
         <TabPanel value="2">
-            <div>
+            <div className='eachlists'>
             {
                 
-                coins.map((coin, index)=> <h1>{index+1}.{coin.id}</h1>)
+                coins.map((coin, index)=> {
+                  return(
+                    <li className='eachcoindesc'>
+                      <img src={coin.image} alt="img" className='image'></img>
+                    <span className='symbol'>{coin.symbol}</span>
+                      <span className='names'>{coin.name}</span >
+                      <span>{coin.id}</span>
+                      {coin.price_change_percentage_24h>0 ?
+                                <div className='numbergroup'>
+                                  <p className='percent greencol'>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+                                  <div><TrendingUpRoundedIcon/></div>
+                                </div>
+                                :
+                                <div className='numbergroup'>
+                                  <p className='percent redcol'>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+                                  <div><TrendingDownRoundedIcon/></div>
+                                </div>
+                            } 
+                    </li>
+                )})
             }
             </div>
             </TabPanel>
