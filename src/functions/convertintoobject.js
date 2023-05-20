861f966198ca77650c0b498ecf9cf9a131a47fa8
Penugonda <sundara.k.penugonda@accenture.com>
2023-05-20 13:32:04 +0530

    rmeove unused

--- src/functions/convertintoobject.js
+++
@@ -1,8 +0,0 @@
-export const convertintoobject=(setCoininfo, coindata)=>{
-    setCoininfo({
-        symbol: coindata.symbol,
-        rank: coindata.coingecko_rank,
-        // image: coindata.image.large,
-        //desc: coindata.description.bg,
-    })
-};
